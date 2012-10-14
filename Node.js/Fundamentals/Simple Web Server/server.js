5b1d342f142d46b8ab9303c41b9387e588798be5
Alejandro Morales <vamg008@gmail.com>
2012-10-14 14:46:45 -0600

    html no es text/plain ;)

--- Node.js/Fundamentals/Simple Web Server/server.js
+++ Node.js/Fundamentals/Simple Web Server/server.js
@@ -12,6 +12,6 @@ var http  = require('http')
   , index = fs.readFileSync('index.html')
 
 http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type' : 'text/plain' })
+  res.writeHead(200, { 'Content-Type' : 'text/html' })
   res.end(index)
 }).listen(8080)
